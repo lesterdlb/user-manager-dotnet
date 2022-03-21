c5600afc34a0f18ced27c0e5acdd5e8299c7056f
Lester <lesterdlb@gmail.com>
2022-03-20 23:26:08 -0600

    Added AdminLTE theme

---
+++ UserManager/Helpers/NavigationIndicatorHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManager.Helpers
+{
+    public static class NavigationIndicatorHelper
+    {
+        public static string MakeActiveClass(this IUrlHelper urlHelper, string controller, string action)
+        {
+            try
+            {
+                var className = "active";
+
+                var controllerName = urlHelper.ActionContext.RouteData.Values["controller"]?.ToString();
+                var actionName = urlHelper.ActionContext.RouteData.Values["action"]?.ToString();
+
+                if (string.IsNullOrEmpty(controllerName)) return string.Empty;
+
+                if (controllerName.Equals(controller, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (actionName is not null && actionName.Equals(action, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return className;
+                    }
+                }
+                return string.Empty;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
