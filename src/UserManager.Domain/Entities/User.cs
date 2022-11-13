de84713ceaa4b744c19a34ff6a7c7881aa4cebb5
Lester <lesterdlb@gmail.com>
2022-11-12 21:15:48 -0600

    Add api project

---
+++ src/UserManager.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManager.Domain.Entities;
+
+public abstract class User
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public Uri? ProfilePicture { get; init; }
+}
---
+++ src/UserManager.Domain/Enums/Roles.cs
@@ -0,0 +1,7 @@
+﻿namespace UserManager.Domain.Enums;
+
+public enum Roles
+{
+    Admin,
+    User,
+}
