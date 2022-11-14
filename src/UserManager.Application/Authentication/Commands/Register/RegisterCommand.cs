07dc1e4de4c178eb86f38502706603f42cecb2d3
Lester <lesterdlb@gmail.com>
2022-11-13 22:19:33 -0600

    Add register command

---
+++ src/UserManager.Application/Authentication/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+using UserManager.Application.Common.Contracts.Authentication;
+
+namespace UserManager.Application.Authentication.Commands.Register;
+
+public record RegisterCommand(RegisterRequest Request) : IRequest<ErrorOr<RegisterResponse>>;
---
+++ src/UserManager.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+using MediatR;
+using UserManager.Application.Common.Contracts.Authentication;
+using UserManager.Application.Common.Interfaces.Services;
+
+namespace UserManager.Application.Authentication.Commands.Register;
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand, ErrorOr<RegisterResponse>>
+{
+    private readonly IIdentityService _identityService;
+
+    public RegisterCommandHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<ErrorOr<RegisterResponse>> Handle(RegisterCommand command, CancellationToken cancellationToken)
+    {
+        return await _identityService.RegisterUserAsync(command.Request);
+    }
+}
