c75a610bb3761e94f7b838ee74c5acb83959abda
Lester <lesterdlb@gmail.com>
2022-11-13 22:10:58 -0600

    Add api dependencies

---
+++ src/UserManager.Api/DependencyInjection.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.OpenApi.Models;
+using UserManager.Api.Services;
+using UserManager.Application.Common.Interfaces.Services;
+
+namespace UserManager.Api;
+
+public static class DependencyInjection
+{
+    public const string CorsPolicy = "CorsPolicy";
+
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+        AddSwagger(services);
+        AddCors(services);
+
+        return services;
+    }
+
+    private static void AddSwagger(IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description =
+                    "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            });
+
+            options.SwaggerDoc("v1",
+                new OpenApiInfo { Title = "UserManager", Version = "v1" });
+        });
+    }
+
+    private static void AddCors(IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(CorsPolicy,
+                policyBuilder => policyBuilder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+    }
+}
---
+++ src/UserManager.Api/Services/CurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using UserManager.Application.Common.Interfaces.Services;
+
+namespace UserManager.Api.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public string? UserId => _httpContextAccessor.HttpContext?.User.FindFirstValue("uid");
+}
