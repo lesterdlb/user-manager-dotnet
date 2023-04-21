51427d7363ed1fcd173bc82a029d42ff4c1f9126
Lester <lesterdlb@gmail.com>
2023-04-20 20:21:43 -0600

    Add validation behaviour class

---
+++ src/UserManager.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using ErrorOr;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace UserManager.Application.Common.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidationBehaviour(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+            return await next();
+
+        return TryCreateResponseFromErrors(validationResult.Errors, out var response)
+            ? response
+            : throw new ValidationException(validationResult.Errors);
+    }
+
+    private static bool TryCreateResponseFromErrors(
+        List<ValidationFailure> validationFailures,
+        out TResponse response)
+    {
+        var errors = validationFailures.ConvertAll(x => Error.Validation(
+            code: x.PropertyName,
+            description: x.ErrorMessage));
+
+        response = (TResponse?)typeof(TResponse)
+            .GetMethod(
+                name: nameof(ErrorOr<object>.From),
+                bindingAttr: BindingFlags.Static | BindingFlags.Public,
+                types: new[] { typeof(List<Error>) })?
+            .Invoke(null, new object?[] { errors })!;
+
+        return response is not null;
+    }
+}
