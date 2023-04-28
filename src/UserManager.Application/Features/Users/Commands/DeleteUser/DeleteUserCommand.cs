76e5a204e47d9cd98e4b65945789aa2d0ea341a8
Lester <lesterdlb@gmail.com>
2023-04-28 14:45:17 -0600

    Add delete user command & handler

---
+++ src/UserManager.Application/Features/Users/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+
+using MediatR;
+
+namespace UserManager.Application.Features.Users.Commands.DeleteUser;
+
+public record DeleteUserCommand(Guid Id) : IRequest<ErrorOr<Unit>>;
---
+++ src/UserManager.Application/Features/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ErrorOr;
+
+using MediatR;
+
+using UserManager.Application.Common.Interfaces.Repositories;
+using UserManager.Domain.Common.Errors;
+
+namespace UserManager.Application.Features.Users.Commands.DeleteUser;
+
+public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, ErrorOr<Unit>>
+{
+    private readonly IUserRepository _userRepository;
+
+    public DeleteUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<ErrorOr<Unit>> Handle(DeleteUserCommand command, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(command.Id);
+
+        if (user is null) return Errors.User.UserNotFound;
+
+        await _userRepository.DeleteAsync(command.Id);
+
+        return Unit.Value;
+    }
+}
