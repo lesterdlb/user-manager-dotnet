0fb7d2a20ce3408ca7b7dc81020b807da56b4553
Lester <lesterdlb@gmail.com>
2023-04-28 14:40:21 -0600

    Add create user command handler

---
+++ src/UserManager.Application/Features/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using ErrorOr;
+
+using MapsterMapper;
+
+using MediatR;
+
+using UserManager.Application.Common.Interfaces.Repositories;
+using UserManager.Domain.Entities;
+
+namespace UserManager.Application.Features.Users.Commands.CreateUser;
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, ErrorOr<Guid>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public CreateUserCommandHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<ErrorOr<Guid>> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+    {
+        var userToCreate = _mapper.Map<User>(command);
+
+        var user = await _userRepository.AddUserWithPasswordAsync(userToCreate, command.Password);
+
+        await _userRepository.AddUserToRoleAsync(user.Id, command.Role);
+
+        return user.Id;
+    }
+}
