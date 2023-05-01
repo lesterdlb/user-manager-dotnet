159acbc1eef6d88d5b3b8d66d430a93d264b6bd3
Lester <lesterdlb@gmail.com>
2023-04-30 19:26:34 -0600

    Add register functionality to mvc application

--- src/UserManager.MVC/Controllers/AuthenticationController.cs
+++ src/UserManager.MVC/Controllers/AuthenticationController.cs
@@ -26,23 +26,37 @@ public class AuthenticationController : Controller
     }
 
     [HttpPost]
-    public async Task<IActionResult> Login(LoginViewModel login, string? returnUrl = null)
+    public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl = null)
     {
         if (ModelState.IsValid)
         {
-            var isSignedIn = await _authenticationService.Login(login.Email, login.Password);
+            var isSignedIn = await _authenticationService.Login(model.Email, model.Password);
             if (isSignedIn)
                 return LocalRedirect(returnUrl ?? Url.Content("~/"));
         }
 
         ModelState.AddModelError(string.Empty, "Invalid login attempt");
-        return View(login);
+        return View(model);
     }
 
     [HttpGet]
     public IActionResult Register()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var isRegistered = await _authenticationService.Register(model);
+            if (isRegistered)
+                return LocalRedirect(Url.Content("~/"));
+        }
+
+        ModelState.AddModelError(string.Empty, "Registration Attempt Failed. Please try again.");
+        return View(model);
     }
 
     [HttpPost]
--- src/UserManager.MVC/Services/AuthenticationService.cs
+++ src/UserManager.MVC/Services/AuthenticationService.cs
@@ -1,10 +1,15 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+
+using MapsterMapper;
+
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+
 using UserManager.MVC.Contracts;
 using UserManager.MVC.Models;
 using UserManager.MVC.Services.Base;
+
 using IAuthenticationService = UserManager.MVC.Contracts.IAuthenticationService;
 
 namespace UserManager.MVC.Services;
@@ -15,17 +20,20 @@ public class AuthenticationService : BaseHttpService, IAuthenticationService
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly JwtSecurityTokenHandler _tokenHandler;
     private readonly ILogger<AuthenticationService> _logger;
+    private readonly IMapper _mapper;
 
     public AuthenticationService(
         IClient client,
         ILocalStorageService localStorageService,
         IHttpContextAccessor httpContextAccessor,
-        ILogger<AuthenticationService> logger) : base(client, localStorageService)
+        ILogger<AuthenticationService> logger,
+        IMapper mapper) : base(client, localStorageService)
     {
         _localStorageService = localStorageService;
         _httpContextAccessor = httpContextAccessor;
         _tokenHandler = new JwtSecurityTokenHandler();
         _logger = logger;
+        _mapper = mapper;
     }
 
     public async Task<bool> Login(string email, string password)
@@ -61,9 +69,22 @@ public class AuthenticationService : BaseHttpService, IAuthenticationService
         return false;
     }
 
-    public Task<bool> Register(RegisterViewModel register)
+    public async Task<bool> Register(RegisterViewModel model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var registerRequest = _mapper.Map<RegisterRequest>(model);
+            var registerResponse = await Client.RegisterAsync(registerRequest);
+
+            if (string.IsNullOrEmpty(registerResponse.UserId)) return false;
+
+            return await Login(model.Email, model.Password);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error while registering");
+            return false;
+        }
     }
 
     public async Task Logout()
